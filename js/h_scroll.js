abbaf247cc39de253b71046c2e5ad2f81275345d
wintain <wintain@naver.com>
2022-03-08 04:17:51 +0900

    H scroll And V scroll is added

---
+++ js/h_scroll.js
@@ -0,0 +1,35 @@
+function Hscroll(hscroll){
+    this.container = hscroll.querySelector(".h_scroll_container");
+    this.containerWidth = this.container.getBoundingClientRect().right - this.container.getBoundingClientRect().left;
+    this.scrollWidth = this.container.scrollWidth;
+    this.colorScrollBar = hscroll.querySelector(".h_scroll_color_bar");
+    this.basicScrollBar = hscroll.querySelector(".h_scroll_basic_bar");
+    this.basicScrollBarWidth = this.basicScrollBar.clientWidth;
+    this.colorScrollBarWidth = this.containerWidth * this.basicScrollBarWidth / this.scrollWidth;
+    initHscrollBar(this);
+    this.container.addEventListener("scroll", moveHscrollBar(this));
+}
+
+function moveHscrollBar(Hscroll){
+    return function (){
+        const xMove = Hscroll.basicScrollBarWidth * Hscroll.container.scrollLeft/Hscroll.scrollWidth;
+        console.log(xMove)
+        Hscroll.colorScrollBar.setAttribute("style", `width:${Hscroll.colorScrollBarWidth}px;transform:translateX(${xMove}px);`)
+    }
+}
+
+function initHscrollBar(Hscroll){
+    Hscroll.colorScrollBar.setAttribute("style", `width:${Hscroll.colorScrollBarWidth}px;`)
+}
+
+const ArrivalHscrollNode = document.querySelector('#arrival_h_scroll');
+const ArrivalHscroll = new Hscroll(ArrivalHscrollNode);
+
+const SubscribeHscrollNode = document.querySelector('#subscribe_h_scroll');
+const SubscribeHscroll = new Hscroll(SubscribeHscrollNode);
+
+const MagazineHscrollNode = document.querySelector("#magazine_h_scroll");
+const MagazineHscroll = new Hscroll(MagazineHscrollNode);
+
+const ReviewHscrollNode = document.querySelector("#review_h_scroll");
+const ReviewHscroll = new Hscroll(ReviewHscrollNode);
---
+++ js/v_scroll.js
@@ -0,0 +1,30 @@
+function Vscroll(vscroll){
+    this.container = vscroll.querySelector(".v_scroll_container");
+    this.containerHeight = this.container.getBoundingClientRect().bottom - this.container.getBoundingClientRect().top;
+    this.scrollHeight = this.container.scrollHeight;
+    this.colorScrollBar = vscroll.querySelector(".v_scroll_color_bar");
+    this.basicScrollBar = vscroll.querySelector(".v_scroll_basic_bar");
+    this.basicScrollBarHeight = this.basicScrollBar.clientHeight;
+    this.colorScrollBarHeight = this.containerHeight * this.basicScrollBarHeight / this.scrollHeight;
+    initVscrollBar(this);
+    this.container.addEventListener("scroll", moveVscrollBar(this));
+}
+
+function moveVscrollBar(Vscroll){
+    return function (){
+        const yMove = Vscroll.basicScrollBarHeight * Vscroll.container.scrollTop/Vscroll.scrollHeight;
+        console.log(yMove)
+        Vscroll.colorScrollBar.setAttribute("style", `height:${Vscroll.colorScrollBarHeight}px;transform:translateY(${yMove}px);`)
+    }
+}
+
+function initVscrollBar(Vscroll){
+    console.log(Vscroll);
+    Vscroll.colorScrollBar.setAttribute("style", `height:${Vscroll.colorScrollBarHeight}px;`)
+}
+
+const VScrollMonth3Node = document.querySelector("#v_scroll_month3");
+const VScrollMonth3 = new Vscroll(VScrollMonth3Node);
+
+const VScrollMonth6Node = document.querySelector("#v_scroll_month6");
+const VScrollMonth6 = new Vscroll(VScrollMonth6Node);
