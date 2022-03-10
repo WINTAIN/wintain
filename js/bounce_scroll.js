456f7f6d494ba4328feb51136fd7d970c0e130fb
wintain <wintain@naver.com>
2022-03-10 23:15:09 +0900

    bounce scroll problem check edit

--- js/bounce_scroll.js
+++ js/bounce_scroll.js
@@ -1,6 +1,5 @@
 function bounceScrollAlert(e){
     const div = e.currentTarget;
-{ // 일반적인 스크롤 방향일 때
     if (div.scrollTop < 0) alert('FUCK!'); // scroll 최상단을 뚫음
     if (div.scrollTop > div.scrollHeight - div.clientHeight) alert('FUCK!'); // scroll 최상단을 뚫음
 }
