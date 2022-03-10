45a9196cb10875639ced34d58b61e1a6b2e54810
wintain <wintain@naver.com>
2022-03-10 23:26:50 +0900

    change bouce_scroll.js

--- js/bounce_scroll.js
+++ js/bounce_scroll.js
@@ -1,9 +1,9 @@
-alert("CHECK BOUNCE SCROLL");
+confirm("CHECK BOUNCE SCROLL");
 
 function bounceScrollAlert(e){
     const div = e.currentTarget;
-    if (div.scrollTop < 0) alert('FUCK!'); // scroll 최상단을 뚫음
-    if (div.scrollTop > div.scrollHeight - div.clientHeight) alert('FUCK!'); // scroll 최상단을 뚫음
+    if (div.scrollTop < 0) confirm('FUCK!'); // scroll 최상단을 뚫음
+    if (div.scrollTop > div.scrollHeight - div.clientHeight) confirm('FUCK!'); // scroll 최상단을 뚫음
 }
 
 document.body.addEventListener("scroll", bounceScrollAlert);
