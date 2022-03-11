93c130ace864792c7b27a34361a49b2f079865a7
wintain <wintain@naver.com>
2022-03-11 16:41:18 +0900

    change css for ios chrome

--- js/h_scroll.js
+++ js/h_scroll.js
@@ -13,8 +13,8 @@ function Hscroll(hscroll){
 
 function setXMove(Hscroll){
     const xMove = Hscroll.basicScrollBarWidth * Hscroll.container.scrollLeft/Hscroll.scrollWidth;
-    if (xMove > Hscroll.basicScrollBarWidth){
-        xMove = Hscroll.basicScrollBarWidth;
+    if (xMove > Hscroll.basicScrollBarWidth-colorScrollBarWidth){
+        xMove = Hscroll.basicScrollBarWidth-colorScrollBarWidth;
     }else if (xMove < 0){
         xMove = 0;
     }
