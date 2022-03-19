0e66b79e775d85b078a4fa36b5b684d36b1cf552
wintain <wintain@naver.com>
2022-03-19 19:49:34 +0900

    all check is added

--- js/auth/joinus_check.js
+++ js/auth/joinus_check.js
@@ -2,6 +2,9 @@ export default initCheck;
 
 
 const allCheckLines = document.querySelectorAll(".check_line");
+const allAccept = document.querySelector("#all_accept");
+
+let allAgree = false;
 
 function changeOnOffSrc(src){
     if (src.indexOf('_on') != -1){
@@ -16,7 +19,35 @@ function handleCheckLineClick(e){
     checkCircle.src = changeOnOffSrc(checkCircle.src);
 }
 
+function check(checkLine){
+    const checkCircle = checkLine.querySelector("img");
+    if (checkCircle.src.indexOf('_off') != -1){
+        checkCircle.src = checkCircle.src.replace("_off", "_on");
+    }
+}
+
+function uncheck(checkLine){
+    const checkCircle = checkLine.querySelector("img");
+    if (checkCircle.src.indexOf('_on') != -1){
+        checkCircle.src = checkCircle.src.replace("_on", "_off");
+    }
+}
+
+function handleAllAccept(e){
+    if (allAgree){
+        for(var i=1;i<allCheckLines.length;i++){
+            uncheck(allCheckLines[i]);
+        }
+    }else{
+        for(var i=1;i<allCheckLines.length;i++){
+            check(allCheckLines[i]);
+        }
+    }
+    allAgree=!allAgree;
+}
+
 function initCheck(){
+    allAccept.addEventListener("click", handleAllAccept);
     for(var i=0; i<allCheckLines.length; i++){
         allCheckLines[i].addEventListener("click", handleCheckLineClick);
     }
